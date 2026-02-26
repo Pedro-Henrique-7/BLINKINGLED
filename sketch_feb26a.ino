de90953f77881965f96387f402975d0b2b21f9d9
PEDRO HENRIQUE OLIVEIRA DA SILVA <25132766@CFP127ALUNO.LOCAL>
2026-02-26 15:31:48 -0300

    Código feito para fazer o led acender e apagar

---
+++ sketch_feb26a.ino
@@ -0,0 +1,15 @@
+// C++ code
+
+void setup()
+{
+  pinMode (23, OUTPUT); //função de saída de pino 23
+}
+
+void loop()
+{
+  digitalWrite(23, HIGH); //acende o led
+  delay(500); //por 2 segundos
+  digitalWrite(23, LOW); //apaga o led
+  delay(500); //por 2 segundos
+}
+
